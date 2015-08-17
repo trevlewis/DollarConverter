4ee64d02c2aecbfcbc355401c1d271a82166275a
Trevor <tdlewis7@gmail.com>
2015-08-16 22:49:59 -0500

    Project Files
    All the files for the project.

---
+++ test/com/converter/ConvertedStringTest.java
@@ -0,0 +1,54 @@
+package com.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConvertedStringTest {
+	
+	Converter converter;
+
+	@Before
+	public void setUp() throws Exception {
+		converter = new Converter();
+	}
+
+	@Test
+	public void test1() {
+		String convertedStr = converter.getConvertedString(999999999.99);
+		assertEquals("Nine hundred ninety-nine million nine hundred ninety-nine thousand "
+				+ "nine hundred ninety-nine and 99/100 dollars", convertedStr);
+	}
+	
+	@Test
+	public void test2() {
+		String convertedStr = converter.getConvertedString(2523.04);
+		assertEquals("Two thousand five hundred twenty-three and 04/100 dollars", convertedStr);
+	}
+	
+	@Test
+	public void test3() {
+		String convertedStr = converter.getConvertedString(13579.9);
+		assertEquals("Thirteen thousand five hundred seventy-nine and 90/100 dollars", convertedStr);
+	}
+	
+	@Test
+	public void test4() {
+		String convertedStr = converter.getConvertedString(246589.00);
+		assertEquals("Two hundred forty-six thousand five hundred eighty-nine and 00/100 dollars", convertedStr);
+	}
+	
+	@Test
+	public void test5() {
+		String convertedStr = converter.getConvertedString(5);
+		assertEquals("Five and 00/100 dollars", convertedStr);
+	}
+	
+	@Test
+	public void test6() {
+		String convertedStr = converter.getConvertedString(99999999.99);
+		assertEquals("Ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine and 99/100 dollars", convertedStr);
+	}
+
+}
